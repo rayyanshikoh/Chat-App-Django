53a79489634e3f2a1d042774fe9cf69b2bd4a65f
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2023-12-22 11:28:00 +0500

    Added css and html

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,11 +6,14 @@ from channels.generic.websocket import WebsocketConsumer
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.username = self.scope['url_route']['kwargs']['username']
+        print(self.room_name, self.username)
         self.room_group_name = f'chat_{self.room_name}'
 
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
-            self.channel_name
+            self.channel_name,
+            # self.username 
         )
         self.accept()
     
@@ -23,18 +26,22 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        sent_by = text_data_json['username']
         
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'sent_by': sent_by
             }
         )
 
     def chat_message(self, event):
         message = event['message']
+        sent_by = event['sent_by']
         
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'username': sent_by
         }))
--- chat/views.py
+++ chat/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'chat/index.html')
 
-def room(request, room_name):
+def room(request, room_name, username):
     return render(request, 'chat/room.html', {
-        'room_name': room_name
+        'room_name': room_name,
+        'username': username
     })
