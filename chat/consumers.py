887175148fc9a4b5acc7dae050518f497e067978
Rayyan Shikoh <75220358+rayyanshikoh@users.noreply.github.com>
2023-12-25 17:49:12 +0500

    Complete code

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
@@ -7,7 +8,6 @@ class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.username = self.scope['url_route']['kwargs']['username']
-        print(self.room_name, self.username)
         self.room_group_name = f'chat_{self.room_name}'
 
         async_to_sync(self.channel_layer.group_add)(
@@ -40,8 +40,33 @@ class ChatConsumer(WebsocketConsumer):
     def chat_message(self, event):
         message = event['message']
         sent_by = event['sent_by']
+        time = datetime.now().strftime("%H:%M")
         
         self.send(text_data=json.dumps({
             'message': message,
-            'username': sent_by
+            'username': sent_by,
+            'time': time
         }))
+
+# A user types a message into the chat box and hits 'Enter' or clicks the 'Send' button. 
+# This triggers a JavaScript function that sends the message and the username of the sender to the server over a WebSocket connection. 
+# The message is sent as a JSON string.
+
+# The server receives the message on the WebSocket and triggers the receive method in the consumer. 
+# The text_data parameter of this method contains the JSON string that was sent by the client.
+
+# Inside the receive method, the JSON string is parsed into a Python dictionary using json.loads(text_data). 
+# The message and the username are extracted from this dictionary.
+
+# The receive method then sends a message to the group associated with the chat room. This is done using async_to_sync(self.channel_layer.group_send). 
+# The message sent to the group includes the type of the message ('chat_message'), the message itself, and the username of the sender.
+
+# The group_send method triggers the chat_message method on all consumers that are in the group. 
+# The event parameter of this method contains the message that was sent to the group.
+
+# Inside the chat_message method, the message and the username are extracted from the event dictionary. The current time is also obtained and formatted as a string.
+
+# The chat_message method then sends a message back to the client over the WebSocket. This is done using self.send. 
+# The message sent back to the client includes the message, the username of the sender, and the time. This message is sent as a JSON string.
+
+# The client receives the message on the WebSocket and updates the chat box to display the new message, the username of the sender, and the time the message was sent.
--- chatapp/settings.py
+++ chatapp/settings.py
@@ -74,13 +74,19 @@ TEMPLATES = [
 ASGI_APPLICATION = 'chatapp.asgi.application'
 WSGI_APPLICATION = 'chatapp.wsgi.application'
 
+# CHANNEL_LAYERS = {
+#     'default': {
+#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+#         'CONFIG': {
+#             'hosts': [('127.0.0.1', 6379)],
+#         },
+#     },
+# }
+
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
-    },
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
 }
 
 
